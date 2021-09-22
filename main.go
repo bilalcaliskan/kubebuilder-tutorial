e90e36579d9ed7a668e1ad54219a373dc49a3cc6
bilalcaliskan <bilalcaliskan@protonmail.com>
2021-09-22 23:28:33 +0300

    add new api

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	batchv1 "github.com/bilalcaliskan/kubebuilder-tutorial/api/v1"
+	"github.com/bilalcaliskan/kubebuilder-tutorial/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -45,6 +48,7 @@ var (
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(batchv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
@@ -75,6 +79,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.CronJobReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "CronJob")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
